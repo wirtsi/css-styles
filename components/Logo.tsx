0f7424550d6faf65b0c6e902d7a3e4e297725039
Florian Krause <florian.krause@etribes.de>
2021-10-25 11:02:30 +0200

    Finish styled-jsx exmaple

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,25 @@
 import React, { FC, HTMLProps } from "react";
 import classnames from "classnames";
 import styles from "./Logo.module.css";
+import homer1 from "../public/homer1.png";
 
-export const Logo: FC<HTMLProps<HTMLLabelElement>> = ({ className }) => {
-  return <div className={classnames(className, styles.logo)}><style jsx>{`
-  
-  background-image: var(--logo);
-  position: absolute;
-  height: 32px;
-  width: 32px;
-  background-size: contain;
-  background-repeat: no-repeat;
-  right: 16px;
-  top: 4px;
-  `}</style></div>;
+export const Logo: FC<HTMLProps<HTMLLabelElement>> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames(className, styles.logo)}>
+      <style jsx>{`
+        background-image: url(${homer1});
+        position: absolute;
+        height: 32px;
+        width: 32px;
+        background-size: contain;
+        background-repeat: no-repeat;
+        right: 16px;
+        top: 4px;
+      `}</style>
+      {children}
+    </div>
+  );
 };
--- styles/variables.ts
+++ styles/variables.ts
@@ -1,1 +1,18 @@
+import homer1 from "../public/homer1.png";
+import homer2 from "../public/homer2.png";
+
 export const buttonColor = '#f3a';
+
+
+export const themes = {
+  light: {
+    bg: "white",
+    color: "black",
+    logo: homer1
+  },
+  dark: {
+    bg: "linear-gradient(#000, #222)",
+    color: "white",
+    logo: homer2 
+  },
+};
